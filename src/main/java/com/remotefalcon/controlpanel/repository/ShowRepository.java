f4d8298d75018b8cb69fe229de407d419730e72e
whitesoup12 <whitesoup12@users.noreply.github.com>
2024-06-21 12:36:37 -0500

    Shows map (#40)

--- src/main/java/com/remotefalcon/controlpanel/repository/ShowRepository.java
+++ src/main/java/com/remotefalcon/controlpanel/repository/ShowRepository.java
@@ -3,8 +3,10 @@ package com.remotefalcon.controlpanel.repository;
 import com.remotefalcon.library.documents.Show;
 import jakarta.transaction.Transactional;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface ShowRepository extends MongoRepository<Show, String> {
@@ -15,4 +17,7 @@ public interface ShowRepository extends MongoRepository<Show, String> {
     Optional<Show> findByEmailOrShowSubdomain(String email, String showSubdomain);
     Optional<Show> findByEmail(String email);
     Optional<Show> findByPasswordResetLinkAndPasswordResetExpiryGreaterThan(String passwordResetLink, LocalDateTime passwordResetExpiry);
+
+    @Query("{ 'preferences.showOnMap' : true }")
+    List<Show> getShowsOnMap();
 }
