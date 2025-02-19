f12f5fbfa90c6f2358a2843ec340de771a7e88bf
whitesoup12 <whitesoup12@users.noreply.github.com>
2025-02-19 16:13:21 -0600

    Change active viewers to playing now (#50)

--- src/main/java/com/remotefalcon/controlpanel/response/dashboard/DashboardLiveStatsResponse.java
+++ src/main/java/com/remotefalcon/controlpanel/response/dashboard/DashboardLiveStatsResponse.java
@@ -6,10 +6,10 @@ import lombok.Data;
 @Builder
 @Data
 public class DashboardLiveStatsResponse {
-    private Integer activeViewers;
-    private Integer totalViewers;
     private Integer currentRequests;
     private Integer totalRequests;
     private Integer currentVotes;
     private Integer totalVotes;
+    private String playingNow;
+    private String playingNext;
 }
